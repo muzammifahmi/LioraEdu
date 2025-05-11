12e322156fcf4ef3de466380031ef661b2569ad6
muzammifahmi <muzammifahmi1@gmail.com>
2025-05-11 22:41:14 +0700

    modif

--- soalquiz/edit_soalquiz.php
+++ soalquiz/edit_soalquiz.php
@@ -43,12 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Edit Soal</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+
 <body>
 <main class="container mt-5">
     <h1>Edit Soal</h1>
